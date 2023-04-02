14ffe07da1b3e02323367bb1ccf9f3e47c2ea51b
JakovlevAS <128615533+JakovlevAS@users.noreply.github.com>
2023-04-02 19:40:32 +0600

    Update main.py
    Added program time

--- main.py
+++ main.py
@@ -4,16 +4,22 @@ from bs4 import BeautifulSoup
 from tkinter import *
 from tkinter import ttk
 import os
+import datetime
 
 def active_label(parent):
     # Create a container frame
     frame = Frame(parent)
 
-
     lbl1 = Label(frame, text='Отправьте ссылку на картинку') #Creates a Label widget with the caption
+    lbl2 = Label(frame, text='Время работы программы: ')
     lbl1.grid(column=0, row=0, padx=10, pady=10)
+    lbl2.grid(column=1, row=1, padx=10, pady=10)
     url_entry = ttk.Entry(frame) #Creates an Entry widget for inputting a URL.
     url_entry.grid(column=0, row=1, padx=10, pady=10)
+
+    # Define the start time for the elapsed time label
+    start_time = datetime.datetime.now()
+
 
     def on_submit(): #Defines the behavior when the "Отправить" button is clicked.
         url_entry_text = url_entry.get()
@@ -21,7 +27,7 @@ def active_label(parent):
         clear(url_entry)
 
     btn1 = Button(frame, text='Отправить', command=on_submit)
-    btn1.grid(column=0, row=2, padx=10, pady=10)
+    btn1.grid(column=0, row=3, padx=10, pady=10)
 
     frame.grid(row=0, column=0, sticky="nsew") #Places the frame widget in a grid layout in row 0 and column 0 and sets the widget to stretch in all directions.
     parent.grid_rowconfigure(0, weight=1)
@@ -29,7 +35,16 @@ def active_label(parent):
     frame.grid_rowconfigure(1, weight=1)
     frame.grid_columnconfigure(0, weight=1)
 
-    return url_entry
+    # Start the label update loop
+    lbl2.after(1000, update_label, lbl2, start_time)
+
+    return url_entry, frame, lbl2
+
+def update_label(lbl2, start_time):
+    delta = str(datetime.datetime.now() - start_time)
+    lbl2.config(text='Время работы программы: ' + delta.split('.')[0])
+    lbl2.after(1000, update_label, lbl2, start_time)
+
 
 
 def clear(url_entry):   #Defines the clear() function to clear the entry widget.
@@ -52,7 +67,7 @@ def parsing(url_entry_text): # Defines the function parsing() to extract informa
         return
 
     soup = BeautifulSoup(response.content, 'html.parser')
-    folder_path = 'D:\\Картинки'
+    folder_path = 'X:\\Картинки'
 
     #The function then checks if a directory exists at the specified path, and creates it if it does not exist.
     try:
@@ -71,7 +86,6 @@ def parsing(url_entry_text): # Defines the function parsing() to extract informa
                     f.write(requests.get(image_url).content)
             except IOError as e:
                 print("Error writing image file:", e)
-
 
 
 if __name__ == '__main__':
